a7f70c9eb9e3af3317fee3fb8f455e249a9124f6
Puttaraj-14 <pputtraj2@gmail.com>
2023-09-19 17:18:37 +0530

    added test run file

--- src/test/java/PageObject/testrun.java
+++ src/test/java/PageObject/testrun.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 public class testrun {
     @RunWith(Cucumber.class)
     @CucumberOptions(features="resources/features", glue="")
-    public class TestRunner_GoogleHomepage {
+    public class TestRunner_Dairypage {
 
     }
 
